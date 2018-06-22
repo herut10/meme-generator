865b3a865bcc31c875a6522b0ee5e1c8c8bdb623
Idan Hakim <idanhakim123@gmail.com>
2018-06-22 12:57:17 +0300

    update

--- js/util-service.js
+++ js/util-service.js
@@ -13,3 +13,11 @@ function makeId(length) {
     }
     return text;
 }
+
+function saveToStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value))
+}
+
+function loadFromStorage(key) {
+    return JSON.parse(localStorage.getItem(key))
+}
