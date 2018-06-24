82d6b763ad10fade98b006797820c57d6d878b93
Herut Yona <yonaherut@gmail.com>
2018-06-24 12:28:26 +0300

    added modal

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ function initMain() {
     renderPopularKeywords();
     setGridDimensions()
     updateHexagonPosition()
+    initModal()
 }
 
 function setGridDimensions() {
@@ -44,7 +45,7 @@ function renderPopularKeywords() {
     var keywords = getPopularKeywords();
     var strHtml = '';
     strHtml = keywords.map(function (keyword) {
-        return `<h1 style="font-size: ${map(keyword.count, 1, gTotalKeywordCount, 0, 4)}em">${keyword.keyword}</h1>`;
+        return `<h1 style="font-size: ${map(keyword.count, 1, gTotalKeywordCount, 0.5, 4)}em">${keyword.keyword}</h1>`;
     }).join('');
 
     document.querySelector('.popular-Keywords-container').innerHTML = strHtml;
@@ -294,4 +295,33 @@ function onSetLang(lang) {
         document.body.classList.remove('rtl');
     }
     translatePage();
+}
+
+
+function initModal() {
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 }
